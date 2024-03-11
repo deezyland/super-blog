684d38735f7c38edec55839a759725e2ef6b5434
Corltand <30843025+deezyland@users.noreply.github.com>
2024-03-10 23:46:51 -0500

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+module.exports = withBundleAnalyzer({});
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  output: "export",
+};
+
+module.exports = nextConfig;
